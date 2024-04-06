eb3b3c696d8c9145fc3f0fd89126b248fa86d9dd
Skywcs1020 <skywcs2001@gmail.com>
2024-04-06 18:52:18 +0800

    Revert "Done simple frontend setup"
    This reverts commit 8916f9f0f7425cc5f0db79df0fd2e4f628cc0952.

--- pages/App.js
+++ pages/App.js
@@ -1,23 +1,34 @@
+<<<<<<< HEAD:pages/App.js
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./HomePage/HomePage";
 import ViewPage from "./ViewPage/ViewPage";
 import UpdatePage from "./UpdatePage/UpdatePage";
 import Navbar from "./component/Navbar/Navbar";
+=======
+import logo from './logo.svg';
+import './App.css';
+>>>>>>> parent of 8916f9f (Done simple frontend setup):src/App.js
 
 function App() {
-    return (
-        <div>
-            <Router>
-                <Navbar />
-                <Routes>
-                    <Route path="/view" element={<ViewPage />} />
-                    <Route path="/update" element={<UpdatePage />} />
-                    <Route path="/" element={<HomePage />} />
-                </Routes>
-            </Router>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
